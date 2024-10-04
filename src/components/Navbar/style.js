2e9b4b2c0806e9a904cfd85288c48e6423862937
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-10-05 01:24:10 +0500

    sidebar parent and child path

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   border: 1px solid coral;
+  height: 60px;
 `;
 
 export { Container };
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,55 +1,80 @@
-import { Outlet } from "react-router-dom";
-import { Container, Side, Body, Wrapper } from "./style";
+/* eslint-disable react/jsx-key */
+import { Outlet, useNavigate } from "react-router-dom";
+import {
+  Container,
+  Side,
+  Body,
+  Wrapper,
+  Logo,
+  LogOut,
+  Menu,
+  MenuItem,
+  Arrow,
+  ChildWrapper,
+} from "./style";
 import { Navbar } from "../Navbar";
+import { Profile } from "./profile";
+import sidebar from "../../utils/sidebar";
+import { useState } from "react";
 
 export const Sidebar = () => {
+  const [open, setOpen] = useState([]);
+
+  const navigate = useNavigate();
+  const onClickLogo = () => {
+    navigate("/");
+  };
+
+  const onLogOut = () => {
+    navigate("/");
+  };
+  const onclickParent = (id) => {
+    if (open.includes(id)) {
+      let data = open.filter((item) => item !== id);
+      setOpen(data);
+    } else {
+      setOpen([...open, id]);
+    }
+
+    console.log(open);
+  };
+
   return (
     <Container>
       <Side>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
-        <h1>Sidebar</h1>
+        <Logo onClick={onClickLogo}>CRM</Logo>
+        <Profile />
+
+        <Menu>
+          {sidebar.map((parent) => {
+            const { icon: Icon } = parent;
+            const active = open.includes(parent.id);
+            return (
+              <>
+                <MenuItem
+                  key={parent.id}
+                  onClick={() => onclickParent(parent.id)}
+                >
+                  <MenuItem.Title>
+                    <Icon className="icon" /> {parent.title}
+                  </MenuItem.Title>
+                  {parent?.children?.length && <Arrow active={active} />}
+                </MenuItem>
+
+                <ChildWrapper active={active}>
+                  {parent?.children?.map((child) => {
+                    return (
+                      <MenuItem key={child?.id}>
+                        <MenuItem.Title>{child.title}</MenuItem.Title>
+                      </MenuItem>
+                    );
+                  })}
+                </ChildWrapper>
+              </>
+            );
+          })}
+        </Menu>
+        <LogOut onClick={onLogOut}>Chiqish</LogOut>
       </Side>
       <Body>
         <Navbar />
