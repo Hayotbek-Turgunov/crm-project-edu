f76c91c8b1c9843522cf43917d9fefa355088647
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2024-10-20 05:03:09 -0700

    generic input done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,22 @@
+import Input from "../Generics/Input";
 import DateTime from "./DataTimePicker";
 import GenericSelect from "./Select";
-import { Container, Input, InputWrapper, SearchIcon, Section } from "./style";
+import { Container, SearchIcon, Section } from "./style";
 import Time from "./Time";
 
 export const Navbar = () => {
   return (
     <Container>
-      <InputWrapper>
-        <SearchIcon />
-        <Input placeholder="Search" />
-      </InputWrapper>
+      <Input
+        width={500}
+        height={40}
+        borderRadius={8}
+        iconLeft={<SearchIcon />}
+      />
       <Section>
-        <GenericSelect />
-        <DateTime />
         <Time />
+        <DateTime />
+        <GenericSelect />
       </Section>
     </Container>
   );
