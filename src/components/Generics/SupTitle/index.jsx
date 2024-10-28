71e8a33f7f7dcfca8d85d24610b83d367d646e13
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2024-10-28 23:04:37 +0500

    analytics-page_4 done

--- src/components/Generics/SupTitle/index.jsx
+++ src/components/Generics/SupTitle/index.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
-import { Container } from "./style";
+import { Container, Counter } from "./style";
 
 export const SupTitle = (props) => {
-  return <Container {...props}>{props?.children}</Container>;
+  return (
+    <Container {...props}>
+      {props?.children} {props?.count && <Counter>{props.count}</Counter>}
+    </Container>
+  );
 };
 
 export default SupTitle;
